e2536bf95806fded8dda529da3c7963b14caa6f1
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-11-19 21:14:21 +0100

    AddStyleClassOnSidebarClick(feat): Displaying at screen sidebar element clicked

--- build/AREA/WebClient/Models/Area.cs
+++ build/AREA/WebClient/Models/Area.cs
@@ -7,13 +7,11 @@ namespace WebClient.Models
 {
     public class Area
     {
-        public bool IsSelected { get; set; }
         public string Name { get; set; }
         public Tree<string> Tree { get; set; }
 
         public Area(string name)
         {
-            IsSelected = false;
             Name = name;
             Tree = new Tree<string>("");
         }
---
+++ build/AREA/WebClient/Scripts/CanvasDrawing.js
@@ -0,0 +1,12 @@
+﻿var canvas = document.getElementById("canvasTree");
+var ctx = canvas.getContext("2d");
+
+function clearCanvasContent()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawTextInCanvas(text, fontSize, fontFamily, posX, posY) {
+    ctx.font = fontSize.toString() + "px " + fontFamily;
+    ctx.fillText(text, posX, posY);
+}
