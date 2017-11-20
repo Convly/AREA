1192c099807aa659199fe7e17e919f3fda021e9d
JSX <jean-sebastien.herbaux@epitech.eu>
2017-11-20 22:35:35 +0100

    ServerBasisImplementation(test): Adding client test for server

---
+++ build/AREA/ClientTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTest
+{
+    class Program
+    {
+        public static string UserName = "Herbaux";
+        public static int Callback(Network.NetTools.Packet obj)
+        {
+            Console.WriteLine("Gotcha");
+            Network.MonitorClient.Instance.SendDataToServer(new Network.NetTools.Packet
+            {
+                Name = UserName,
+                Key = obj.Key,
+                Data = new KeyValuePair<Network.NetTools.PacketCommand, object>(Network.NetTools.PacketCommand.C_UNLOCK, null)
+            });
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Network.MonitorClient monitorClient = Network.MonitorClient.Instance;
+            monitorClient.Start("Monitor", Callback, args[0], int.Parse(args[1]));
+            monitorClient.SendDataToServer(new Network.NetTools.Packet { Name = UserName , Key = 0, Data = new KeyValuePair<Network.NetTools.PacketCommand, object>(Network.NetTools.PacketCommand.C_REGISTER, "rootz") });
+            Console.ReadKey();
+        }
+    }
+}
