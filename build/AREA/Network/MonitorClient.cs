a09c0a58f38f570503a0f4417e38a924968928df
JSX <jean-sebastien.herbaux@epitech.eu>
2017-11-20 22:33:03 +0100

    ServerBasisImplementation(feat): Adding Network library

---
+++ build/AREA/Network/MonitorClient.cs
@@ -0,0 +1,117 @@
+﻿using NetworkCommsDotNet;
+using NetworkCommsDotNet.Connections;
+using System;
+using Newtonsoft.Json;
+
+namespace Network
+{
+    /// <summary>
+    /// Client class for the Network library.
+    /// This class allow the user to start a transmission in order to receive and send requests to a single <see cref="Server"/>.
+    /// </summary>
+    public class MonitorClient
+    {
+        private static MonitorClient instance = null;
+
+        /// <summary>
+        /// Getter for the singleton instance of a <see cref="MonitorClient"/>
+        /// </summary>
+        public static MonitorClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MonitorClient();
+                }
+                return instance;
+            }
+        }
+
+        private MonitorClient() { }
+
+        /// <summary>
+        /// Destructor of the <see cref="MonitorClient"/>. When called, it'll shutdown all the transmissions
+        /// </summary>
+        ~MonitorClient()
+        {
+            try
+            {
+                NetworkComms.Shutdown();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        private string _serverIP = null;
+        private int _serverPort;
+
+        /// <summary>
+        /// Static callback method for the server request
+        /// </summary>
+        public static Func<NetTools.Packet, int> CallBackFct;
+        /// <summary>
+        /// Static name for the communication channel 
+        /// </summary>
+        public static string Channel;
+
+        /// <summary>
+        /// Launch the ClientNetwork
+        /// </summary>
+        /// <param name="channel">Name of the channel which will be used for the communication</param>
+        /// <param name="callBackFct">Function called when the client receive a message from the server</param>
+        /// <param name="serverIP">Ip of the server you want to connect to</param>
+        /// <param name="serverPort">Port of the server you want to connect to</param>
+        public void Start(string channel, Func<NetTools.Packet, int> callBackFct, string serverIP, int serverPort)
+        {
+            try
+            {
+                if (_serverIP == null)
+                {
+                    _serverIP = serverIP;
+                    _serverPort = serverPort;
+                    CallBackFct = callBackFct;
+                    Channel = channel;
+                    NetworkComms.AppendGlobalIncomingPacketHandler<string>(Channel, ServerRequest);
+                    Connection.StartListening(ConnectionType.TCP, new System.Net.IPEndPoint(System.Net.IPAddress.Any, 0));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Send an object to the server (you will minimum need "public string name" in your object
+        /// </summary>
+        /// <param name="data"></param>
+        public void SendDataToServer(Object data)
+        {
+            try
+            {
+                NetworkComms.SendObject(Channel, _serverIP, _serverPort, JsonConvert.SerializeObject(data));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Trigered function when the server want to communicate with the Monitor
+        /// </summary>
+        /// <param name="header"></param>
+        /// <param name="connection"></param>
+        /// <param name="data">Data send by the server</param>
+        public static void ServerRequest(PacketHeader header, Connection connection, string data)
+        {
+            Console.WriteLine("New request received from the server");
+            NetTools.Packet dataObject = JsonConvert.DeserializeObject<NetTools.Packet>(data);
+
+            CallBackFct(dataObject);
+        }
+    }
+}
---
+++ build/AREA/Network/NetTools/Packet.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Network.NetTools
+{
+    /// <summary>
+    /// Enum used to defined the type of the <see cref="Packet"/>'s <see cref="Object"/>
+    /// </summary>
+    public enum PacketCommand
+    {
+        /// <summary>
+        /// Communication direction: Client => Server.  
+        /// Request a register from a Client to the Server.  
+        /// The object associated to the command must be a <see cref="string"/> (which is the server's password).  
+        /// </summary>
+        C_REGISTER,
+        /// <summary>
+        /// Communication direction: Client => Server.  
+        /// Request a register from a Client to the Server.  
+        /// The object associated to the command must be a <see cref="string"/> (which is the server's password).  
+        /// </summary>
+        C_QUIT,
+        /// <summary>
+        /// Communication direction: Client => Server.  
+        /// Request a register from a Client to the Server.  
+        /// The object associated to the command must be a <see cref="string"/> (which is the server's password).  
+        /// </summary>
+        C_UNLOCK,
+        /// <summary>
+        /// Communication direction: Both.  
+        /// Inform a network entity about an error that occured.  
+        /// The object associated to the command must be a <see cref="string"/>.  
+        /// </summary>
+        ERROR // 
+    }
+
+    /// <summary>
+    /// Class used to transfer data across the network.
+    /// </summary>
+    public class Packet
+    {
+        private string name;
+        private uint key;
+        private KeyValuePair<PacketCommand, Object> data;
+
+
+        /// <summary>
+        /// Default constructor for a <see cref="Packet"/>
+        /// </summary>
+        public Packet()
+        {
+
+        }
+
+        /// <summary>
+        /// Main constructor for client side. The key is initialized to 0 and the registration state to false.
+        /// </summary>
+        /// <param name="name_">The name of the emitter.</param>
+        /// <param name="command_">The command associated to <paramref name="data_"/></param>
+        /// <param name="data_">The <see cref="Object"/> which contain the data</param>
+        public Packet(string name_, PacketCommand command_, Object data_)
+        {
+            this.Name = name_;
+            this.Key = 0;
+            this.Data = new KeyValuePair<PacketCommand, Object>(command_, data_);
+        }
+
+        /// <summary>
+        /// Main constructor for server side, the key must be initialized with a value.
+        /// </summary>
+        /// <param name="name_">The name of the emitter. For server side, it will be "root"</param>
+        /// <param name="key_">The value of the Locker key</param>
+        /// <param name="command_">The command associated to <paramref name="data_"/></param>
+        /// <param name="data_">The <see cref="Object"/> which contain the data</param>
+        public Packet(string name_, uint key_, PacketCommand command_, Object data_)
+        {
+            this.Name = name_;
+            this.Key = key_;
+            this.Data = new KeyValuePair<PacketCommand, Object>(command_, data_);
+        }
+
+        /// <summary>
+        /// Access the name of the packet's author
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+        /// <summary>
+        /// Access the <see cref="Lock.Locker"/>'s key of the <see cref="Packet"/>
+        /// </summary>
+        public uint Key { get => key; set => key = value; }
+        /// <summary>
+        /// Access the Data of the <see cref="Packet"/>
+        /// </summary>
+        public KeyValuePair<PacketCommand, object> Data { get => data; set => data = value; }
+    }
+}
