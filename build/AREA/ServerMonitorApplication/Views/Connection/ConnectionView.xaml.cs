ba95ee4220a89b3626200fca027d450526e9bcf2
UnlimitedGitHub <33202250+UnlimitedGitHub@users.noreply.github.com>
2017-11-22 15:21:12 +0100

    AddingLoginPage(feat): Adding new mvvm pattern
    Connection and Login page added

---
+++ build/AREA/ServerMonitorApplication/Views/Connection/ConnectionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ServerMonitorApplication.Views.Connection
+{
+    /// <summary>
+    /// Logique d'interaction pour ConnectionView.xaml
+    /// </summary>
+    public partial class ConnectionView : UserControl
+    {
+        public ConnectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
