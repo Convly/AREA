7d666354cc44d19e6c03adf2c99e49c98c4310eb
UnlimitedGitHub <33202250+UnlimitedGitHub@users.noreply.github.com>
2017-11-23 10:39:25 +0100

    LoginInputsManagement(feat): username and password management

--- build/AREA/ServerMonitorApplication/Views/Login/LoginViewModel.cs
+++ build/AREA/ServerMonitorApplication/Views/Login/LoginViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using System.Windows;
 
 namespace ServerMonitorApplication
 {
     public class LoginViewModel : ViewModelBase
     {
+        #region Properties
+
+        /// <summary>
+        /// Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Password unsecured
+        /// </summary>
+        public string Password { get; set; }
+
+        #endregion
+
         #region Commands
 
         /// <summary>
@@ -18,11 +33,23 @@ namespace ServerMonitorApplication
         #region Private Methods
 
         /// <summary>
+        /// Inputs verfication
+        /// </summary>
+        /// <returns></returns>
+        private bool CanShowServicesView()
+        {
+            return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
+        }
+
+        /// <summary>
         /// Send the new page to display on the main window
         /// </summary>
         private void ShowServicesView()
         {
-            Messenger.Default.Send(new ChangePage(typeof(ServicesViewModel)));
+            if (CanShowServicesView())
+            {
+                Messenger.Default.Send(new ChangePage(typeof(ServicesViewModel)));
+            }
         }
 
         #endregion
