ee9d596b5f76ec657b8951960b4d918ddc51bc40
Marc-Antoine Leconte <marc-antoine.leconte@epitech.eu>
2017-11-25 22:41:25 +0100

    ServiceIntegration(feat): starting threadPool in service

--- build/AREA/Services/FacebookService/GraphAPI.cs
+++ build/AREA/Services/FacebookService/GraphAPI.cs
@@ -4,21 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Network.NetTools;
+using System.Threading;
+
+public class ThreadPool
+{
+    private Dictionary<string, Thread> _threads;
+
+    public Dictionary<string, Thread>   Threads { get; set; }
+
+    public void Add(string reaction, void() func)
+    {
+        Thread newThread = new Thread(new ThreadStart(func));
+
+        newThread.Start();
+        _threads.Add(reaction, newThread);
+    }
+
+    public void Remove(string action)
+    {
+        _threads[action].Abort();
+        _threads.Remove(action);
+    }
+}
 
 namespace FacebookService
 {
     public class GraphAPI : Network.NetTools.IService
     {
-        private static string name = "graphAPI";
+        private static string   _name = "graphAPI";
+        private ThreadPool      _threadPool;
 
         public static void Callback(Packet obj)
         {
-            Console.WriteLine("Hello from " + name);
+            Console.WriteLine("Hello from " + _name);
         }
 
         public string APIName()
         {
-            return name;
+            return (_name);
         }
 
         public List<string> GetActionList()
