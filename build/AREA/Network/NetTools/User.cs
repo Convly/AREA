071a1c597d1dedf70889d2c50af0fdf1dd12b978
Codex04 <quentin.metge@hotmail.fr>
2017-11-26 16:03:46 +0100

    ServerIntegrationWebClient(fix): add commentaries

--- build/AREA/Network/NetTools/User.cs
+++ build/AREA/Network/NetTools/User.cs
@@ -10,6 +10,11 @@ namespace Network.NetTools
     /// </summary>
     public class User
     {
+        /// <summary>
+        /// Consructor of User
+        /// </summary>
+        /// <param name="email">email</param>
+        /// <param name="pwd">password</param>
         public User(string email, string pwd)
         {
             Email = email;
@@ -17,23 +22,48 @@ namespace Network.NetTools
             LastUpdated = null;
         }
 
+        /// <summary>
+        /// return the state of the token
+        /// </summary>
+        /// <returns></returns>
         public bool AreTokensExpired()
         {
             return (LastUpdated == null || DateTime.UtcNow.Subtract((DateTime)LastUpdated).TotalMinutes >= 120.0f);
         }
 
+        /// <summary>
+        /// User's Id
+        /// </summary>
         public ObjectId Id { get; set; }
+
+        /// <summary>
+        /// User's email
+        /// </summary>
         [BsonElement("Email")]
         public string Email { get; set; }
+
+        /// <summary>
+        /// User's pwd
+        /// </summary>
         [BsonElement("Pwd")]
         public string Pwd { get; set; }
 
         private Dictionary<string, string> accessToken;
+        /// <summary>
+        /// Get the token access
+        /// </summary>
         public Dictionary<string, string> AccessToken { get => accessToken; set => accessToken = value; }
 
         private Dictionary<string, string> accessTokenSecret;
+
+        /// <summary>
+        /// Get the secret token access
+        /// </summary>
         public Dictionary<string, string> AccessTokenSecret { get => accessTokenSecret; set => accessTokenSecret = value; }
 
+        /// <summary>
+        /// Get the last update of the token
+        /// </summary>
         public Nullable<DateTime> LastUpdated { get; set; }
     }
 }
