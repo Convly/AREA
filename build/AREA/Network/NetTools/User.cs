f4c25edc10825685d019a1b953d1694be68659e1
Maxime Cauvin <maxime.cauvin@epitech.eu>
2017-11-26 18:53:48 +0100

    WebClientDocumentation(feat): Documentation for the WebClient done

--- build/AREA/Network/NetTools/User.cs
+++ build/AREA/Network/NetTools/User.cs
@@ -10,6 +10,11 @@ namespace Network.NetTools
     /// </summary>
     public class User
     {
+        /// <summary>
+        /// Constructor for <see cref="User"/>
+        /// </summary>
+        /// <param name="email">The <see cref="User"/>'s email</param>
+        /// <param name="pwd">The <see cref="User"/>'s password</param>
         public User(string email, string pwd)
         {
             Email = email;
@@ -17,23 +22,49 @@ namespace Network.NetTools
             LastUpdated = null;
         }
 
+        /// <summary>
+        /// Check if <see cref="User"/>'s tokens are expired
+        /// </summary>
+        /// <returns>A <see cref="bool"/> if tokens are expired or not</returns>
         public bool AreTokensExpired()
         {
             return (LastUpdated == null || DateTime.UtcNow.Subtract((DateTime)LastUpdated).TotalMinutes >= 120.0f);
         }
 
+        /// <summary>
+        /// The <see cref="ObjectId"/> <see cref="User"/>'s identifier
+        /// </summary>
         public ObjectId Id { get; set; }
+
+        /// <summary>
+        /// The <see cref="User"/>'s email
+        /// </summary>
         [BsonElement("Email")]
         public string Email { get; set; }
+
+        /// <summary>
+        /// The <see cref="User"/>'s password
+        /// </summary>
         [BsonElement("Pwd")]
         public string Pwd { get; set; }
 
         private Dictionary<string, string> accessToken;
+
+        /// <summary>
+        /// The <see cref="User"/>'s standard tokens
+        /// </summary>
         public Dictionary<string, string> AccessToken { get => accessToken; set => accessToken = value; }
 
         private Dictionary<string, string> accessTokenSecret;
+
+        /// <summary>
+        /// The <see cref="User"/>'s secret tokens
+        /// </summary>
         public Dictionary<string, string> AccessTokenSecret { get => accessTokenSecret; set => accessTokenSecret = value; }
 
+        /// <summary>
+        /// The last updated <see cref="DateTime"/> for the <see cref="User"/>'s tokens
+        /// </summary>
         public Nullable<DateTime> LastUpdated { get; set; }
     }
 }
